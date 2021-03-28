a19906925a124b9651bebdce4ac9a8e39aad25c1
patsyleung <81509354+patsyleung@users.noreply.github.com>
2021-03-28 17:36:43 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,56 @@
+let roll = 0
+input.onGesture(Gesture.Shake, function () {
+    roll = randint(1, 6)
+    if (roll == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (roll == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . . . . .
+            . . # . .
+            . . . . .
+            `)
+    } else if (roll == 3) {
+        basic.showLeds(`
+            # . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . #
+            `)
+    } else if (roll == 4) {
+        basic.showLeds(`
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            `)
+    } else if (roll == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            . # . # .
+            . . . . .
+            . # . # .
+            . . . . .
+            . # . # .
+            `)
+    }
+})
+basic.forever(function () {
+	
+})
